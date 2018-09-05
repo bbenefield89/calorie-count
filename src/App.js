85f04f566717e0be4ec2d15494182f107d7d5900
bbenefield89 <bsquared18@gmail.com>
2018-09-05 17:58:30 -0400

    CONDITIONALLY RENDERS LOG IN/OUT FORMS
    - src/App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,81 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+
+import NavBar from './components/misc/NavBar';
+import LoggedInForm from './components/LoggedInForm';
+import LoggedOutForm from './components/LoggedOutForm';
+
 import './App.css';
 
 class App extends Component {
+  state = {
+    loggedIn: false,
+    authToken: ''
+  }
+  
+  // showLoginForm
+  showLoginForm = () => {
+    this.setState({ loggedIn: true });
+  }
+
+  // hideLoginForm
+  hideLoginForm = () => {
+    this.setState({ loggedIn: false });
+  }
+
+  // submitLoginForm
+  submitLoginForm = inputVals => {
+    const request = {
+      method: 'POST',
+      url: '/api-token-auth/',
+      data: inputVals
+    }
+
+    axios(request)
+      .then(({ data }) => {
+        const { token } = data;
+        localStorage.setItem('token', token)
+
+        const authToken = localStorage.getItem('token');
+        this.setState({ authToken });
+      })
+      .catch(err => console.log(err));
+  }
+
+  // componentDidMount
+  componentDidMount() {
+    const authToken = localStorage.getItem('token');
+
+    if (authToken)
+      this.setState({ authToken });
+    else
+      this.hideLoginForm();
+  }
+  
+  // render
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <React.Fragment>
+        <NavBar
+          loggedIn      = { this.state.loggedIn }
+          showLoginForm = { this.showLoginForm }
+        />
+
+        <h1>Welcome to Calorie Count</h1>
+        <p>The single easiest solution for well... counting calories!</p>
+
+        {
+          this.state.authToken
+          ?
+            <h1>RETURN NUTRITION</h1>
+          :
+            this.state.loggedIn
+            ?
+              <LoggedInForm submitLoginForm={ this.submitLoginForm } />
+            :
+              <LoggedOutForm />
+        }
+      </React.Fragment>
     );
   }
 }
